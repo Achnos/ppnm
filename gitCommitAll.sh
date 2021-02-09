814165b130146b3e9dd9359e6720c4460258cf08
Marc <achnms3@gmail.com>
2021-02-09 17:44:23 +0100

    'test'

--- gitCommitAll.sh
+++ gitCommitAll.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Standard simple shell script to quickly commit all changes
+
+echo "Enter your commit to this push: "
+read  COMMIT
+
 git init
 git add --all
-git commit --all -m 'Initial commit'
+git commit --all -m $COMMIT
 git push
